274f3540bf34e245bcd755d5d81db3d22ec28d2f
mk <mbpereyra@gmail.com>
2025-07-13 16:03:20 -0300

    Actualice README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Challenge-Pereyra
+
+## Instrucciones para ejecutar la aplicacion:
+
+## Backend:
+en la terminal escribir:
+cd backend
+npm install
+node index.js
+Se abre en: http://localhost:3000
+
+## Frontend:
+en la terminal escribir:
+cd frontend
+npm install
+npm run dev
+se abre en:http://localhost:5173
+
+# Screenshots:
+
+![Inicio](./screenshots/Inicio.jpg)
+
+![Aplicación en ejecución](./screenshots/aplicacion-en-ejecucion.png)
--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -3,26 +3,26 @@ import TaskItem from './TaskItem';
 import { Link } from 'react-router-dom';
 
 function TaskList() {
-  const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {
-    fetch('/api/tasks')
-      .then(res => res.json())
-      .then(data => setTasks(data))
-      .catch(() => alert('Error al cargar tareas'));
-  }, []);
+    useEffect(() => {
+        fetch('/api/tasks')
+            .then(res => res.json())
+            .then(data => setTasks(data))
+            .catch(() => alert('Error al cargar tareas'));
+    }, []);
 
-  return (
-    <div style={{ padding: '20px', backgroundColor: '#f5f0ff' }}>
-      <h1 style={{ color: 'purple' }}>Lista de Tareas</h1>
-      <Link to="/new" style={{ color: 'purple', textDecoration: 'none' }}>Crear nueva tarea</Link>
-      <ul style={{ listStyle: 'none', padding: 0 }}>
-        {tasks.map(task => (
-          <TaskItem key={task.id} task={task} />
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div style={{ padding: '20px', backgroundColor: '#f5f0ff' }}>
+            <h1 style={{ color: 'purple' }}>Lista de Tareas</h1>
+            <Link to="/new" style={{ color: 'purple', textDecoration: 'none' }}>Crear nueva tarea</Link>
+            <ul style={{ listStyle: 'none', padding: 0 }}>
+                {tasks.map(task => (
+                    <TaskItem key={task.id} task={task} />
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default TaskList;
